334cd05761c87eb74159db7792e05ea86e9ba673
vickyadrii <vickyadri103@gmail.com>
2025-07-20 17:20:25 +0700

    chore: setup tailwind and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React application.</p>
+
+      <Button>Test</Button>
+    </div>
+  )
+}
+
+export default App;
